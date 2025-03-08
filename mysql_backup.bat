91c73d60e79fd9afeb59fc27bf3a3cd7c2bccbb2
MufaroDKaseke <mufarodkaseke@gmail.com>
2025-03-08 10:21:49 +0200

    Updated to allow return to original state after backup

--- mysql_backup.bat
+++ mysql_backup.bat
@@ -5,7 +5,8 @@ set mysql_bin=C:\xampp\mysql\bin
 set date_stamp=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 set filename=backup_%date_stamp%.sql
 set full_path=%backup_path%\%filename%
-set s3_bucket=s3://<your-bucket-name>/mysql-backups/
+set s3_bucket=s3://momo-db-backups/mysql-backups/
+set mysql_was_stopped=false
 
 :: Variables for MySQL check
 setlocal enabledelayedexpansion
@@ -21,12 +22,13 @@ if %ERRORLEVEL%==0 (
     goto DONE
 ) else (
     echo MySQL is not running. Starting MySQL...
+    set mysql_was_stopped=true
     cd /d "C:\xampp\mysql\bin"
     start mysqld.exe
 )
 
 :: Wait for a few seconds before checking again
-timeout /t 5 >nul
+timeout /t 10 >nul
 set /a COUNT+=1
 
 :: Exit if max attempts are reached
@@ -62,6 +64,11 @@ if %errorlevel% neq 0 (
 :: Delete old backups
 forfiles /p "%backup_path%" /s /m *.sql /d -7 /c "cmd /c del @file"
 
+:: Stop MySQL if it wasn't running before
+if "%mysql_was_stopped%"=="true" (
+    echo Stopping MySQL...
+    taskkill /F /IM mysqld.exe
+)
 :: Display complete message and exit
 echo Backup completed successfully!
 exit /b 0
